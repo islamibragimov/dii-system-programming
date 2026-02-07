8b016b23e4dbcc492a1c8ac51e9329c10dc9a89d
islamibragimov <islamjonibragimov@gmail.com>
2026-02-07 14:51:05 +0500

    ツ

--- ex-1-5.c
+++ ex-1-5.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+int main()
+{
+    for (float fahr = 300; fahr >= 0; fahr = fahr - 20) {
+        float cel = 5*(fahr - 32)/9;
+        printf("%f\t%f\n", fahr, cel);
+    } 
+
+}
+
+
+// in exercise 1.5 the step is not given so i took it as 20 
+
+/*
+// here i wrote the code with our while bro
+#include <stdio.h>
 
 int main()
 {
@@ -11,3 +26,5 @@ int main()
         fahr = fahr - 20;
     }
 }
+
+*/
