96a4b3793b41af1203be5f02b5a83190a98aac2b
islamibragimov <islamjonibragimov@gmail.com>
2026-03-11 01:00:15 +0500

    ツ

--- chapter-4/ex-4-1.c
+++ chapter-4/ex-4-1.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-
 int strindex(char s[], char t[]);
 
 int main(){
-    char s[] = "abcdefcdef";
-    char t[] = "cdef";
-    printf("%d", strindex(s,t));
+    printf("%d", strindex("abcdebcd","bcd"));
 }
 
-int strindex(char s[], char t[]) {
-    int i, j, k;
-
-    for (i = 0; s[i] != '\0'; i++) {
-        // We use j to track 's' starting from 'i', and k for 't' starting from 0
-        for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+int strindex(char s[], char t[]){
+    int i,j,k;
+    for (i=0; s[i] != '\0'; i++){
+        for (j=i, k=0; t[k]!='\0' && s[j] != t[k]; j++, k++){
             ;
-        
-        // If we reached the end of t (t[k] == '\0'), we found a full match!
-        if (k > 0 && t[k] == '\0') {
-            return i;
         }
-    } 
-    return -1;
+        
+
+    }
 }
 
---
+++ chapter-4/ex-4-10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+void reverse(char s[], int k1, int k2);
+
+int main()
+{
+    char s[] = "Aminjon";
+    reverse(s, 0, 6);
+    printf("%s", s);
+    return 0;
+}
+
+
+void reverse(char s[], int k1, int k2){
+    if (k2 >= k1){
+        char temp = s[k1];
+        s[k1] = s[k2];
+        s[k2] = temp;
+        reverse(s, k1 + 1, k2 - 1);
+    }
+}
+
+
+
+
